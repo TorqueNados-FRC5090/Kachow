fd269d02526904461f93432e6704d4b5dfb284ed
cadynD <156255695+cadynD@users.noreply.github.com>
2026-02-10 18:04:49 -0500

    Merge branch 'main' of https://github.com/TorqueNados-FRC5090/Kachow

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
+
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
---
+++ src/main/java/frc/robot/subsystems/Spindex.java
@@ -0,0 +1,22 @@
+
+    package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.wrappers.GenericPID;
+import frc.robot.wrappers.LimitSwitch;
+
+public class Spindex extends SubsystemBase{
+
+    public void Spindex(){
+        
+    }
+
+}
+
+
