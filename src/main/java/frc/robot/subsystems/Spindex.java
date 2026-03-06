54e6324836487012a3083943e4e96e1914954afd
eden <3thereal3den@gmail.com>
2026-03-06 18:14:56 -0500

    spinxdex

--- src/main/java/frc/robot/subsystems/Spindex.java
+++ src/main/java/frc/robot/subsystems/Spindex.java
@@ -1,22 +1,39 @@
-
-    package frc.robot.subsystems;
+package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.controls.Follower;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.wrappers.GenericPID;
-import frc.robot.wrappers.LimitSwitch;
-
-public class Spindex extends SubsystemBase{
-
-    public void Spindex(){
+public class Spindex extends SubsystemBase {
+    TalonFX spindex; 
+    TalonFX accelterator; 
+    public Spindex(){
+        spindex = new TalonFX(13, "upper");
+        accelterator = new TalonFX(14, "upper");
         
+        Slot0Configs spinConfig = new Slot0Configs();
+        spinConfig.kP = .1;
+        accelterator.getConfigurator().apply(spinConfig);
+        spindex.getConfigurator().apply(spinConfig);
+        
+        
+        accelterator.setControl(new Follower(13, MotorAlignmentValue.Opposed));
     }
+//Set Speed and Acceleration
+    public void spin(double spinspeed){
+        spindex.set(spinspeed);
 
+    }
+//Sets Speed and Acceleration to
+    public void spindexStop(){
+        spindex.set(0);
+    } 
+     @Override
+    public void periodic() {
+        SmartDashboard.putNumber("accellator", accelterator.getPosition().getValueAsDouble());
+    }
 }
-
-
